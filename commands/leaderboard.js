948eb9b026bdf3aa7a8d240de9b5daff90874075
SapphireVideos <mateo.hideg@hotmail.com>
2021-01-11 03:32:31 -0300

    Initial commit

--- commands/leaderboard.js
+++
@@ -1,5 +0,0 @@
-exports.run = (client, message, args) => {
-    message.channel.send("Here you go! https://soon.soon/leaderboard/" + message.guild.id + " 🎩");
-};
-
-exports.category = "Rank";
--- commands/resetPrefix.js
+++
@@ -1,3 +0,0 @@
-// THIS FILE WAS CREATED JUST SO THIS COMMAND APEPARS IN HELP
-exports.run = (client, message, args) => {};
-exports.category = "Settings";
